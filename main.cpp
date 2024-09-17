c25af57f10e07cda624c51ab674d1273511c6a8d
Sedim14 <159864676+Sedim14@users.noreply.github.com>
2024-09-17 10:42:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,24 +13,23 @@
 
 std::vector<Mesh*>models;
 
-//Put obj files paths in here
 std::vector<const char*>objs={
     "zorua.obj",
+    "torus.obj",
     "ico.obj",
     "cone.obj",
-    "torus.obj"
 };
 
 
 GLFWwindow* window;
 
 //Frame counter
-float frames = 0;
+int frames = 0;
 
 //Time managers
 float timeAcumulator = 0;
-float timeSlice = 1000; //Time elapse between updates
-const int TARGET_FPS = 20;
+float timeSlice = 60; //Time elapse between updates
+const int TARGET_FPS = 4000;
 const float TARGET_FRAME_TIME = 1000.0f / TARGET_FPS; // Frame time in milliseconds
 bool canLimitFRameRate = false;
 
@@ -53,85 +52,15 @@ std::vector<Vector3D> colorList = {
 
 };
 
+
+
+
+Mesh model;
+
 //Window Height
 float width = 800.0f;
 float height = 600.0f;
 float steps = 0.2f;
-
-
-
-
-void drawText(const char *text,int lenght,int x, int y)
-{
-	glMatrixMode(GL_PROJECTION);
-	double *matrix = new double[16];
-	glGetDoublev(GL_PROJECTION_MATRIX,matrix);
-	glLoadIdentity();
-	glOrtho(0,800,0,600,-5,5);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glPushMatrix();
-	glLoadIdentity();
-	glRasterPos2i(x,y);
-
-	for(int i=0;i<lenght;i++)
-		glutBitmapCharacter(GLUT_BITMAP_9_BY_15,(int)text[i]);
-
-	glPopMatrix();
-	glMatrixMode(GL_PROJECTION);
-	glLoadMatrixd(matrix);
-	glMatrixMode(GL_MODELVIEW);
-}
-
-
-
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-
-	switch(key)
-	{
-		//H
-		case GLFW_KEY_A: eye.x+=steps; break;
-		case GLFW_KEY_S: eye.y+=steps; break;
-		case GLFW_KEY_D: eye.z+=steps; break;
-		case GLFW_KEY_Z: eye.x-=steps; break;
-		case GLFW_KEY_X: eye.y-=steps; break;
-		case GLFW_KEY_C: eye.z-=steps; break;
-		//Centro = hacia donde ve la camara
-		case GLFW_KEY_H: center.x+=steps; break;
-		case GLFW_KEY_J: center.y+=steps; break;
-		case GLFW_KEY_K: center.z+=steps; break;
-		case GLFW_KEY_B: center.x-=steps; break;
-		case GLFW_KEY_N: center.y-=steps; break;
-		case GLFW_KEY_M: center.z-=steps; break;
-
-		//eye
-		case GLFW_KEY_Q: up.x+=steps; break;
-		case GLFW_KEY_W: up.y+=steps; break;
-		case GLFW_KEY_E: up.z+=steps; break;
-		case GLFW_KEY_I: up.x-=steps; break;
-		case GLFW_KEY_O: up.y-=steps; break;
-		case GLFW_KEY_P: up.z-=steps; break;
-
-	}
-    
-}
-
-
-float getCurrentTimeMS()
-{
-	  // Get the current time from the system clock
-    auto now = std::chrono::high_resolution_clock::now();
-
-    // Convert the current time to time since epoch
-    auto duration = now.time_since_epoch();
-
-    // Convert duration to milliseconds
-    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-
-    return static_cast<float>(milliseconds);;
-}
-
 
 void loadModels() {
 
@@ -146,57 +75,140 @@ void loadModels() {
 }
 
 
+
+void drawText(const char* text, int lenght, int x, int y)
+{
+    glDisable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    double* matrix = new double[16];
+    glGetDoublev(GL_PROJECTION_MATRIX, matrix);
+    glLoadIdentity();
+    glOrtho(0, 800, 0, 600, -5, 5);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
+    glLoadIdentity();
+    glRasterPos2i(x, y);
+
+    for (int i = 0; i < lenght; i++)
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, (int)text[i]);
+
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixd(matrix);
+    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_DEPTH_TEST);
+}
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+    switch (key)
+    {
+        //H
+    case GLFW_KEY_A: eye.x += steps; break;
+    case GLFW_KEY_S: eye.y += steps; break;
+    case GLFW_KEY_D: eye.z += steps; break;
+    case GLFW_KEY_Z: eye.x -= steps; break;
+    case GLFW_KEY_X: eye.y -= steps; break;
+    case GLFW_KEY_C: eye.z -= steps; break;
+        //Centro = hacia donde ve la camara
+    case GLFW_KEY_H: center.x += steps; break;
+    case GLFW_KEY_J: center.y += steps; break;
+    case GLFW_KEY_K: center.z += steps; break;
+    case GLFW_KEY_B: center.x -= steps; break;
+    case GLFW_KEY_N: center.y -= steps; break;
+    case GLFW_KEY_M: center.z -= steps; break;
+
+        //eye
+    case GLFW_KEY_Q: up.x += steps; break;
+    case GLFW_KEY_W: up.y += steps; break;
+    case GLFW_KEY_E: up.z += steps; break;
+    case GLFW_KEY_I: up.x -= steps; break;
+    case GLFW_KEY_O: up.y -= steps; break;
+    case GLFW_KEY_P: up.z -= steps; break;
+
+    }
+
+}
+
+
+float getCurrentTimeMS()
+{
+    // Get the current time from the system clock
+    auto now = std::chrono::high_resolution_clock::now();
+
+    // Convert the current time to time since epoch
+    auto duration = now.time_since_epoch();
+
+    // Convert duration to milliseconds
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+
+    return static_cast<float>(milliseconds);;
+}
+
 void display(void)
 {
 
-	//start drawing the frame
-	/*  clear all pixels  */
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //start drawing the frame
+    /*  clear all pixels  */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// set camera view
-	glLoadIdentity();
-	gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
-	
-	for(int i = 0;i<models.size();i++)
-	{
-		models[i]->drawMesh(colorList);
-	}
-	
+    // set camera view
+    glLoadIdentity();
+    gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
+
+    for(int i = 0; i<objs.size();i++){
+        //glPushMatrix();
+        models[i]->drawMesh(colorList);
+        //glPopMatrix();
+    }
+
+
 }
 
 void init(void)
 {
-	/*  select clearing (background) color       */
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+    /*  select clearing (background) color       */
+    glClearColor(0.0, 0.0, 0.0, 0.0);
 
-	/*  Set up projection matrix  */
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0, width / height, 0.1, 100.0);
+    /*  Set up projection matrix  */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, width / height, 0.1, 100.0);
 
-	//glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-	//gluOrtho2D(0.0, 1.0, 0.0, 1.0);
+    //glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    //gluOrtho2D(0.0, 1.0, 0.0, 1.0);
 
-	
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
 
-	//gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-	glEnable(GL_DEPTH_TEST);
+    //gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
+    glEnable(GL_DEPTH_TEST);
 
 }
 
 
 void gameLoop()
 {
-	auto lastUpdate = std::chrono::steady_clock::now();
+    int maxFrameRate = 0;
+
+    auto lastUpdate = std::chrono::steady_clock::now();
+
+    auto lastTime = std::chrono::steady_clock::now();
+
+    int sec=0;
 
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window))
     {
         auto frameStart = std::chrono::steady_clock::now(); // Start time of the frame
-        
+
         // Calculate deltaTime
         auto currentTime = std::chrono::steady_clock::now();
         std::chrono::duration<float, std::milli> delta = currentTime - lastUpdate;
@@ -211,6 +223,7 @@ void gameLoop()
         // Time accumulation for fixed timestep
         timeAcumulator += deltaTime;
 
+
         while (timeAcumulator > timeSlice)
         {
             timeAcumulator -= timeSlice;
@@ -219,22 +232,35 @@ void gameLoop()
         // Rendering
         display();
 
+        //Calculate framerate
+        frames++;
 
-        // Calculate FPS after drawing everything before going to the next frame
-        frames = 1000.0f / deltaTime;
-
-        //show fps on screen
-	    std::stringstream ss;
-	    ss<<static_cast<int>(frames);
-	    std::string strfps = ss.str();
-	    //std::cout<<strfps<<std::endl;
-
-	    drawText(strfps.data(),strfps.size(),0,0);
-
-        // Frame limiter: Wait if the frame was rendered too quickly
+        // Calculate frame duration
         auto frameEnd = std::chrono::steady_clock::now();
         std::chrono::duration<float, std::milli> frameDuration = frameEnd - frameStart;
         float frameTime = frameDuration.count();
+
+
+        // check if it has passed one second
+        auto actualTime = std::chrono::steady_clock::now();
+        //Calculate how many time has pass
+        std::chrono::duration<float, std::milli> deltaMS = actualTime - lastTime;
+
+        //Restart processed frames
+        if(deltaMS.count() > 1000.0f){
+          maxFrameRate = frames;
+          lastTime = actualTime;
+          frames = 0;
+        }
+
+        //Show frame rate
+        std::stringstream ss;
+        ss << "FPS:" << static_cast<int>(maxFrameRate); //
+        std::string strfps = ss.str();
+        drawText(strfps.data(), strfps.size(), 10, 10);
+
+
+
         //Limit the frame rate
         if (frameTime < TARGET_FRAME_TIME && canLimitFRameRate)
         {
@@ -246,6 +272,9 @@ void gameLoop()
         glFlush();
         glfwPollEvents();
     }
+
+    // Clean up and exit
+    glfwDestroyWindow(window);
 
     glfwTerminate();
 }
@@ -259,35 +288,34 @@ void gameLoop()
  */
 int main(int argc, char** argv)
 {
-	/*
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(width, height);
-	glutInitWindowPosition(100, 100);
-	mainWindow = glutCreateWindow("MeshRenderer");*/
+    /*
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(100, 100);
+    mainWindow = glutCreateWindow("MeshRenderer");*/
 
 
-	glutInit(&argc, argv);
+    glutInit(&argc, argv);
 
-	glfwInit();
-	window = glfwCreateWindow(width, height, "meshRenderer", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glfwSetKeyCallback(window, key_callback);
-	
-	
-	loadModels();
+    glfwInit();
+    window = glfwCreateWindow(width, height, "meshRenderer", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
 
-	
-	
-	//glutDisplayFunc(display);
-	//glutIdleFunc(display);
-	//glutKeyboardFunc(handleKeypress);
-	
-	//glutMainLoop();
-	glfwSetKeyCallback(window, key_callback);
-	init();
 
-	gameLoop();
+    loadModels();
 
-	return 0;   /* ISO C requires main to return int. */
+
+    //glutDisplayFunc(display);
+    //glutIdleFunc(display);
+    //glutKeyboardFunc(handleKeypress);
+
+    //glutMainLoop();
+    glfwSetKeyCallback(window, key_callback);
+    init();
+
+    gameLoop();
+
+    return 0;   /* ISO C requires main to return int. */
 }
